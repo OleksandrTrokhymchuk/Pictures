5913fa1813cbce7303e29ab8cd27c3a3a4e60e05
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-13 00:51:49 +0300

    style: add wrapper for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import "../styles/reset.css"
 import DateInfo from "./components/DateInfo"
 import Images from "./components/Images"
+import mainStyles from "@/styles/main/main.module.css"
 
 export default function Home() {
   return (
-    <>
+    <div className={mainStyles["wrapper"]}>
       <DateInfo/>
       <Images/>
-    </>
+    </div>
   )
 }
