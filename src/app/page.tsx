0a896d003b0aa34e5f5652e17c6fef3d9f5432f1
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-12 17:01:49 +0300

    feat: add all images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
+import Image from "next/image"
 import "../styles/reset.css"
+import Image1 from "../../public/images/task-2/1.jpg"
+import Image2 from "../../public/images/task-2/2.jpg"
+import Image3 from "../../public/images/task-2/3.jpg"
+import Image4 from "../../public/images/task-2/4.jpg"
+import Image5 from "../../public/images/task-2/5.jpg"
+import Image6 from "../../public/images/task-2/6.jpg"
+import Image7 from "../../public/images/task-2/7.jpg"
+import Image8 from "../../public/images/task-2/8.jpg"
+import Image9 from "../../public/images/task-2/9.jpg"
+import Image10 from "../../public/images/task-2/10.jpg"
+import Image11 from "../../public/images/task-2/11.jpg"
+import Image12 from "../../public/images/task-2/12.jpg"
 
 export default function Home() {
   return (
     <>
-    
+      <Image src={Image1} alt="text"/>
+      <Image src={Image2} alt="text"/>
+      <Image src={Image3} alt="text"/>
+      <Image src={Image4} alt="text"/>
+      <Image src={Image5} alt="text"/>
+      <Image src={Image6} alt="text"/>
+      <Image src={Image7} alt="text"/>
+      <Image src={Image8} alt="text"/>
+      <Image src={Image9} alt="text"/>
+      <Image src={Image10} alt="text"/>
+      <Image src={Image11} alt="text"/>
+      <Image src={Image12} alt="text"/>
     </>
   )
 }
