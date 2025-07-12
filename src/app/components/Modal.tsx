c0288dd3a66e5102c6e171ab8d426e1900f8d8ae
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-13 02:38:02 +0300

    fix: block scrolling when modal is on

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -71,7 +71,15 @@ export default function Modal({ isOpen, ImageNumber, onClose }: ModalProps) {
 
     const modalOverlayRef = useRef<HTMLDivElement>(null) 
 
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden'
+        } else {
+            document.body.style.overflow = ''
+        }
 
+        return () => {document.body.style.overflow = ''}
+      }, [showModal])
 
     return(
         <>
