12337f8b391eaad00331af9c39f0bf0ce37af814
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-12 17:30:30 +0300

    fix: fix images import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,22 +12,35 @@ import Image9 from "../../public/images/task-2/9.jpg"
 import Image10 from "../../public/images/task-2/10.jpg"
 import Image11 from "../../public/images/task-2/11.jpg"
 import Image12 from "../../public/images/task-2/12.jpg"
+import imageStyles from "../styles/images.module.css"
+
+const imagesData = [
+  { src: require("../../public/images/task-2/1.jpg").default, alt: "American Truck" },
+  { src: require("../../public/images/task-2/2.jpg").default, alt: "Container Port" },
+  { src: require("../../public/images/task-2/3.jpg").default, alt: "Plane" },
+  { src: require("../../public/images/task-2/4.jpg").default, alt: "Mercedes-Benz Truck" },
+  { src: require("../../public/images/task-2/5.jpg").default, alt: "Farm Truck" },
+  { src: require("../../public/images/task-2/6.jpg").default, alt: "Containers" },
+  { src: require("../../public/images/task-2/7.jpg").default, alt: "Volvo Truck" },
+  { src: require("../../public/images/task-2/8.jpg").default, alt: "Volvo Truck" },
+  { src: require("../../public/images/task-2/9.jpg").default, alt: "Ship at sea" }, 
+  { src: require("../../public/images/task-2/10.jpg").default, alt: "Iveco Trucks on highway" }, 
+  { src: require("../../public/images/task-2/11.jpg").default, alt: "Cargo Ship at sunset" }, 
+  { src: require("../../public/images/task-2/12.jpg").default, alt: "Ship in port" }, 
+];
 
 export default function Home() {
   return (
-    <>
-      <Image src={Image1} alt="American Truck"/>
-      <Image src={Image2} alt="Container Port"/>
-      <Image src={Image3} alt="Plane"/>
-      <Image src={Image4} alt="Mercedes-Benz Truck"/>
-      <Image src={Image5} alt="Farm Truck"/>
-      <Image src={Image6} alt="Containers"/>
-      <Image src={Image7} alt="Volvo Truck"/>
-      <Image src={Image8} alt="Volvo Truck"/>
-      <Image src={Image9} alt="Ship"/>
-      <Image src={Image10} alt="Iveco Trucks"/>
-      <Image src={Image11} alt="Ship"/>
-      <Image src={Image12} alt="Ship"/>
-    </>
+    <div className={imageStyles["image__container"]}> 
+      {imagesData.map((img, index) => (
+        <div key={index} className={imageStyles["galleryItem"]}> 
+          <Image
+            className={imageStyles["galleryImage"]} 
+            src={img.src}
+            alt={img.alt}
+          />
+        </div>
+      ))}
+    </div>
   )
 }
