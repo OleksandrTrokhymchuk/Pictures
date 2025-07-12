ceef569c21a3d06da1f038100b1dea125ff6f081
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-13 01:14:29 +0300

    fix: use ES module imports for images

--- src/app/components/Images.tsx
+++ src/app/components/Images.tsx
@@ -6,21 +6,34 @@ import mainStyles from "@/styles/main/main.module.css"
 import { useEffect, useState } from "react"
 import Modal from "./Modal"
 
+import img1 from "../../../public/images/task-2/1.jpg"
+import img2 from "../../../public/images/task-2/2.jpg"
+import img3 from "../../../public/images/task-2/3.jpg"
+import img4 from "../../../public/images/task-2/4.jpg"
+import img5 from "../../../public/images/task-2/5.jpg"
+import img6 from "../../../public/images/task-2/6.jpg"
+import img7 from "../../../public/images/task-2/7.jpg"
+import img8 from "../../../public/images/task-2/8.jpg"
+import img9 from "../../../public/images/task-2/9.jpg"
+import img10 from "../../../public/images/task-2/10.jpg"
+import img11 from "../../../public/images/task-2/11.jpg"
+import img12 from "../../../public/images/task-2/12.jpg"
+
 export default function Images() {
 
     const imagesData = [
-        { id: 1, src: require("../../../public/images/task-2/1.jpg").default, alt: "American Truck" },
-        { id: 2, src: require("../../../public/images/task-2/2.jpg").default, alt: "Container Port" },
-        { id: 3, src: require("../../../public/images/task-2/3.jpg").default, alt: "Plane" },
-        { id: 4, src: require("../../../public/images/task-2/4.jpg").default, alt: "Mercedes-Benz Truck" },
-        { id: 5, src: require("../../../public/images/task-2/5.jpg").default, alt: "Farm Truck" },
-        { id: 6, src: require("../../../public/images/task-2/6.jpg").default, alt: "Containers" },
-        { id: 7, src: require("../../../public/images/task-2/7.jpg").default, alt: "Volvo Truck" },
-        { id: 8, src: require("../../../public/images/task-2/8.jpg").default, alt: "Volvo Truck" },
-        { id: 9, src: require("../../../public/images/task-2/9.jpg").default, alt: "Ship at sea" }, 
-        { id: 10, src: require("../../../public/images/task-2/10.jpg").default, alt: "Iveco Trucks on highway" }, 
-        { id: 11, src: require("../../../public/images/task-2/11.jpg").default, alt: "Cargo Ship at sunset" }, 
-        { id: 12, src: require("../../../public/images/task-2/12.jpg").default, alt: "Ship in port" }, 
+        { id: 1, src: img1, alt: "American Truck" },
+        { id: 2, src: img2, alt: "Container Port" },
+        { id: 3, src: img3, alt: "Plane" },
+        { id: 4, src: img4, alt: "Mercedes-Benz Truck" },
+        { id: 5, src: img5, alt: "Farm Truck" },
+        { id: 6, src: img6, alt: "Containers" },
+        { id: 7, src: img7, alt: "Volvo Truck" },
+        { id: 8, src: img8, alt: "Volvo Truck" },
+        { id: 9, src: img9, alt: "Ship at sea" }, 
+        { id: 10, src: img10, alt: "Iveco Trucks on highway" }, 
+        { id: 11, src: img11, alt: "Cargo Ship at sunset" }, 
+        { id: 12, src: img12, alt: "Ship in port" }, 
     ]
 
     const [displayedImages, setDisplayedImages] = useState<any[]>([])
