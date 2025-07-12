3cd353838d64622cf93e6c4b9c592b0c1aeabd3a
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-12 19:11:26 +0300

    feat: add date info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import "../styles/reset.css"
+import DateInfo from "./components/DateInfo"
 import Images from "./components/Images"
 
 export default function Home() {
   return (
     <>
+      <DateInfo/>
       <Images/>
     </>
   )
